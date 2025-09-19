62aaa02dec23df1ccb2bef38317055d038f7e105
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2025-09-19 02:20:00 +0000

    Add custom og images

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,6 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
+import { customBlogImage } from "./quartz/util/custom-og"
 
 /**
  * Quartz 4 Configuration
@@ -90,8 +91,13 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({
+        colorScheme: "lightMode",
+        width: 1200,
+        height: 630,
+        excludeRoot: false,
+        imageStructure: customBlogImage,
+      }),
     ],
   },
 }
